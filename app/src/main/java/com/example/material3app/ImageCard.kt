3ed447ef392bbafc122aa3c92223cb1396b31eec
aldrinjunior <aldrinjunior@gmail.com>
2022-06-23 14:33:57 -0300

    create Image card and assist chips

--- app/src/main/java/com/example/material3app/ImageCard.kt
+++ app/src/main/java/com/example/material3app/ImageCard.kt
@@ -1,13 +1,23 @@
 package com.example.material3app
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material.icons.outlined.Share
+import androidx.compose.material3.*
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
+import com.google.accompanist.flowlayout.FlowRow
+import com.google.accompanist.flowlayout.SizeMode
 import kotlin.random.Random
 
 @ExperimentalMaterial3Api
@@ -27,7 +37,48 @@ fun ImageCard(
         Image(
             painter = rememberAsyncImagePainter(
                 model = "https://picsum.photos/seed/${Random.nextInt()}/300/200"
-            ), contentDescription = null
+            ),
+            contentDescription = null,
+            modifier = Modifier
+                .clip(MaterialTheme.shapes.large)
+                .fillMaxWidth()
+                .aspectRatio(3f / 2f)
         )
+
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(text = title, style = MaterialTheme.typography.titleLarge)
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(text = description, style = MaterialTheme.typography.bodyMedium)
+            Spacer(modifier = Modifier.height(8.dp))
+            FlowRow(
+                modifier = Modifier.fillMaxWidth(), mainAxisSpacing = 8.dp, mainAxisSize = SizeMode.Wrap
+            ) {
+                AssistChip(
+                    onClick = { /*TODO*/ },
+                    colors = AssistChipDefaults.assistChipColors(leadingIconContentColor = MaterialTheme.colorScheme.onSurfaceVariant),
+                    leadingIcon = {
+                        Icon(imageVector = Icons.Outlined.FavoriteBorder, contentDescription = null)
+                    },
+                    label = { Text(text = "Mark as favorite") }
+                )
+                AssistChip(
+                    onClick = { /*TODO*/ },
+                    colors = AssistChipDefaults.assistChipColors(leadingIconContentColor = MaterialTheme.colorScheme.onSurfaceVariant),
+                    leadingIcon = {
+                        Icon(imageVector = Icons.Outlined.Share, contentDescription = null)
+                    },
+                    label = { Text(text = "Share with others") }
+                )
+            }
+        }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
+@Composable
+fun preview() {
+    ImageCard(title = "Teste", description = "Teste")
 }
